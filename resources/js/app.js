6cddd233dcb53c98c9b54ba993d890dd6d2f92cc
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-06-23 12:20:44 +0200

    add: sass, bootstrap and vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-import './bootstrap';
+import "./bootstrap";
+
+import "~resources/scss/app.scss";
+
+import.meta.glob(["../img/**"]);
+
+import * as bootstrap from "bootstrap";
--- resources/views/benvenuti.blade.php
+++ resources/views/benvenuti.blade.php
@@ -4,15 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
+    @vite('resources/js/app.js')
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <title>Laravel</title>
 </head>
-
 <body>
-
     <h1>BENVENUTI SUU:</h1>
     <h2>Clicca qui sotto per scoprirlo:</h2>
     <a href="laravel">http://127.0.0.1:8000/laravel</a>
